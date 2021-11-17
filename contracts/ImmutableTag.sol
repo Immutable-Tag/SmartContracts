04805a0f6461a361b2c0869882f60f6e8320f987
Vasudha Jha <reachvasudha27@gmail.com>
2021-11-16 21:06:53 -0800

    add mapping for holding all tags

--- contracts/ImmutableTag.sol
+++ contracts/ImmutableTag.sol
@@ -7,4 +7,14 @@ contract ImmutableTag {
         string tagID;
         string commitHash;
     }
+
+    // key for tags is "{repoURL}_{tagID}" for O(1) lookup
+    // example: tags["https://github.com/Immutable-Tag/SmartContacts_v1.0.0"] will return a Tag object
+    // and that Tag object will look like:
+    // {
+    //     repoURL: "https://github.com/Immutable-Tag/SmartContacts"
+    //     tagID: "v1.0.0"
+    //     commitHash: "66190b9fc987cb12c3a302c84123122e68ef6450"
+    // }
+    mapping(string => Tag) public tags;
 }
